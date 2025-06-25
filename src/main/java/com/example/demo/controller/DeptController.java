3e5097bca30480100204a971dd1cb63f55c9f260
jeonhj1015 <mumu097@naver.com>
2025-06-25 15:11:29 +0900

    create DeptController

---
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+	@GetMapping("/board/list")
+	public String list() {
+		return "게시물 목록";
+	}
+}
